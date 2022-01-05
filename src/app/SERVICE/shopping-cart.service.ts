2144e9144c95530ee1bd248a3e04260bab049a19
kavindumadushanka972 <kavindumadushanka972@gmail.com>
2022-01-05 11:33:48 +0530

    add to cart part done

--- src/app/SERVICE/shopping-cart.service.ts
+++ src/app/SERVICE/shopping-cart.service.ts
@@ -5,9 +5,26 @@ import { Injectable } from '@angular/core';
 })
 export class ShoppingCartService {
 
+  shopping_cart_items: any[] = [];
+
   constructor() { }
   addProduct = (product: any) => {
-    console.log(product)
-    // localStorage.setItem('shopping_cart', product)
+    let items = this.get_shopping_cart_items()
+    if(items){ // if already there are items, push product to it and store
+      items.push(product)
+      console.log(items)
+      localStorage.setItem('shopping_cart', JSON.stringify(items)) 
+    }else{ // if there are no items already, add it to shopping_cart_items array and store it
+      this.shopping_cart_items.push(product);
+      console.log(this.shopping_cart_items)
+      localStorage.setItem('shopping_cart', JSON.stringify(this.shopping_cart_items))
+    }
+    
+    
+  }
+
+  get_shopping_cart_items = () => {
+    let items:any = localStorage.getItem('shopping_cart')
+    return JSON.parse(items)
   }
 }
