99a5da13b39b1c79e0eb7ad2538d9f5ae1a948ba
Kavindu Madushanka <kavindumadushanka972@gmail.com>
2022-01-06 23:37:29 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   firebase: {
-    projectId: 'myclone-f4875',
-    appId: '1:923257638175:web:0533f045d8113607d0742a',
-    storageBucket: 'myclone-f4875.appspot.com',
-    apiKey: 'AIzaSyAkc4L5Y-Dqkh2VCbDNlLl4UW3to7Y0uuc',
-    authDomain: 'myclone-f4875.firebaseapp.com',
-    messagingSenderId: '923257638175',
-    measurementId: 'G-WEMRX5GQG0',
+    projectId: '************************',
+    appId: '****************************************',
+    storageBucket: '**********************************',
+    apiKey: '******************************************',
+    authDomain: '***********************************',
+    messagingSenderId: '************',
+    measurementId: '*****************',
   },
   production: false
 };
